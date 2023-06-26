b283a98599dafb440c3bf22f0e1a373e7611ec73
dat <datpham.01032000@gmail.com>
2023-06-26 10:42:55 +0700

    fix homejs

--- js/renderHome.js/home.js
+++ js/renderHome.js/home.js
@@ -13,49 +13,42 @@ function getProduct() {
 }
 
 function hienthiUI(listProduct) {
-  const boxProduct = document.getElementById("Products");
+  const boxProduct = document.getElementById("box-content");
   const content = listProduct.map((shoes) => {
-    const { image, name, price, description } = shoes;
-    return ` 
-    <div class="container">
-      <h1>Products</h1>
+    const { id, image, name, price, shortDescription, myParam } = shoes;
 
-      <div class="box">
-        <div class="row">
-          <div class="col-3">
-            <div class="card">
-              <div class="small_card">
-                <i class="fa-solid fa-heart"></i>
-                <i class="fa-solid fa-share"></i>
-              </div>
-
-              <div class="image">
-                <a href="http://127.0.0.1:5501/detail.html" target="_blank"
-                  ><img src="${image}"
-                /></a>
-              </div>
-
-              <div class="products_text">
-                <h2>${name}</h2>
-                <p>${description}</p>
-                <h3>$${price}</h3>
-
-                <div class="products_star">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
+    return `
+            <div class="col-3">
+              <div class="card">
+                <div class="small_card">
+                  <i class="fa-solid fa-heart"></i>
+                  <i class="fa-solid fa-share"></i>
                 </div>
-                <a href="#" class="btn">Add To Cart</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     
-    `;
+                <div class="image">
+                  <a href="./detail.html?product=${id}"  ><img src="${image}"
+                  /></a>
+                </div>
+    
+                <div class="products_text">
+                  <a href="./detail.html?product=${id}" >
+                  <h2>${name}</h2>
+                  </a>
+                  
+                  <p>${shortDescription}</p>
+                  <h3>$${price}</h3>
+    
+                  <div class="products_star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                  </div>
+                  <a href="./detail.html?product=${id}" class="btn" onclick="hienthiUI(${myParam})">Add To Cart</a>
+                </div>
+              </div>
+            </div>`;
   });
 
   boxProduct.innerHTML = content.join("");
